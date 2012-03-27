41c378f08abb4fded3fad4dffb761151a88e6549
Garami Gábor <hron@hron.me>
2012-03-28 01:11:52 +0200

    Matcher fix

--- lib/i18n-spec/matchers/have_legacy_interpolations.rb
+++ lib/i18n-spec/matchers/have_legacy_interpolations.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :have_legacy_interpolations do |expected|
   match do |actual|
     locale_file = I18nSpec::LocaleFile.new(actual)
-    locale_file.content =~ /{{.+}}/
+    locale_file.content =~ /\{\{.+\}\}/
   end
 end
