e8352d20a0504b2e16957fad1f3a4af6dbe18605
Christopher Dell <chris@tigrish.com>
2012-09-14 22:22:46 +0200

    Add the iso gem as a dependency

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source :rubygems
 
+gem "iso"
+
 group :development do
   gem "rspec", ">= 2.4.0"
-  gem "bundler", "~> 1.1.3"
+  gem "bundler", "~> 1.2.0"
   gem "jeweler", "~> 1.6.4"
   gem "rdoc"
   gem "rcov", ">= 0" if RUBY_VERSION < '1.9'
--- lib/i18n-spec.rb
+++ lib/i18n-spec.rb
@@ -2,6 +2,7 @@ if defined?(Psych) and defined?(Psych::VERSION) and !defined?(YAML::ParseError)
   YAML::ParseError = Psych::SyntaxError
 end
 
+require 'iso'
 Dir[File.dirname(__FILE__) + '/i18n-spec/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/i18n-spec/matchers/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/i18n-spec/shared_examples/*.rb'].each {|file| require file }
