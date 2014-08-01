a10785eacd511fa3fd274e945e5780b5fce7f263
Cédric Félizard <cedric@felizard.fr>
2014-08-01 09:27:07 +0000

    Be explicit when expose_dsl_globally = false (RSpec 3)
    https://relishapp.com/rspec/rspec-core/v/3-0/docs/configuration/global-namespace-dsl

--- lib/i18n-spec/shared_examples/valid_locale_file.rb
+++ lib/i18n-spec/shared_examples/valid_locale_file.rb
@@ -1,4 +1,4 @@
-shared_examples_for "a valid locale file" do |locale_file|
+RSpec.shared_examples_for "a valid locale file" do |locale_file|
   describe locale_file do
     it { should be_parseable }
     it { should have_valid_pluralization_keys }
