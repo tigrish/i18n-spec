06e23f4c6be7c93702a447af3f72f7453e4a6986
Timo Schilling <timo@schilling.io>
2014-10-25 14:16:22 +0200

    fix failure_for_should and failure_for_should_not

--- lib/i18n-spec/failure_message.rb
+++ lib/i18n-spec/failure_message.rb
@@ -1,22 +1,22 @@
 module I18nSpec
   module FailureMessage
-    def failure_for_should
+    def failure_for_should(&block)
       if respond_to?(:failure_message)
         # Rspec 3
-        failure_message { |f| yield(f) }
+        failure_message { |f| instance_exec(f, &block) }
       else
         # Rspec 2
-        failure_message_for_should { |f| yield(f) }
+        failure_message_for_should { |f| instance_exec(f, &block) }
       end
     end
 
-    def failure_for_should_not
+    def failure_for_should_not(&block)
       if respond_to?(:failure_message_when_negated)
         # Rspec 3
-        failure_message_when_negated { |f| yield(f) }
+        failure_message_when_negated { |f| instance_exec(f, &block) }
       else
         # Rspec 2
-        failure_message_for_should_not { |f| yield(f) }
+        failure_message_for_should_not { |f| instance_exec(f, &block) }
       end
     end
   end
